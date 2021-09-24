73805806e11fe1a59d2b24cf6fecff77625b0a64
GrimGuyMike <ggm_job@mail.ru>
2021-09-24 16:33:38 +0500

    preparing client for redesign

--- client/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ text, color, onClick }) => {
-    return (
-        <button style={{ backgroundColor: color }} onClick={onClick} className='btn-add' >
-            <h3 style={{ margin: '0' }}>{text}</h3>
-        </button>
-    );
-};
-
-export default Button;
--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer>
-            <p>Copyright &copy; 2021 | Created with React</p>
-        </footer>
-    )
-};
-
-export default Footer;
