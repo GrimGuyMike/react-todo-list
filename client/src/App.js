b4041e39425ba091a13b9ad42c8e092dbfb78f95
GrimGuyMike <ggm_job@mail.ru>
2021-09-25 17:21:43 +0500

    SignForm component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Header from "./components/Header";
+import SignForm from "./components/SignForm";
 
 function App() {
+
+  const authenticated = useSelector(state => state.auth.authenticated);
 
   return (
     <div className='App'>
       <Header />
+
+      <div id="content">
+        {!authenticated && <SignForm />}
+      </div>
     </div>
   );
 
