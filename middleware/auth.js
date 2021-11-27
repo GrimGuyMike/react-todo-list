1320bec07446cb55e6e18e0e79f9166c77c0ccd6
GrimGuyMike <ggm_job@mail.ru>
2021-11-27 14:15:41 +0500

    authentication middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,13 @@
+const { ApiError } = require('../exception');
+const tokenService = require('../service/token');
+
 module.exports = function(req, res, next) {
+    const header = req.header('Authorization');
+    const token = header?.split(' ')[1];
+    if(!token) throw ApiError.Unauthorized('access token is absent');
+
+    const claims = tokenService.verifyAccess(token);
+    req.user = claims;
+
     next();
 };
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const { ApiError } = require('../exception');
 
-module.exports = function(err, req, res) {
+module.exports = function(err, req, res, next) {
     if(err instanceof ApiError) {
         res.status(err.status).json({ message: err.message });
     } else {
