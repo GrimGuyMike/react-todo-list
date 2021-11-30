046621e36f489e8d8e6c6dd25a0df680c24ac0e5
GrimGuyMike <ggm_job@mail.ru>
2021-11-30 13:59:32 +0500

    client: todoReducer sorting function

--- client/src/state/reducers/todoReducer.js
+++ client/src/state/reducers/todoReducer.js
@@ -6,6 +6,7 @@ import {
     TODOS_LOADING,
     ERASE_TODOS
 } from "../actions/types";
+import sortTodos from "./utils/sortTodos";
 
 const initialState = {
     todos: [],
@@ -26,83 +27,39 @@ const todoReducer = function(state=initialState, action){
             };
 
         case FETCH_TODOS: {
-            let done = action.payload.filter(todo => todo.done);
-            let undone = action.payload.filter(todo => !todo.done);
-            
-            done = done.sort((a, b) => {
-                let aTime = (new Date(a.date)).getTime();
-                let bTime = (new Date(b.date)).getTime();
-                return bTime - aTime;
-            });
-
-            undone = undone.sort((a, b) => {
-                let aTime = (new Date(a.date)).getTime();
-                let bTime = (new Date(b.date)).getTime();
-                return bTime - aTime;
-            });
-
             return {
-                todos: [...undone, ...done],
+                todos: sortTodos(action.payload),
                 loading: false
             };
         }
 
         case ADD_TODO: {
-            const newTodo = action.payload;
-            let done = state.todos.filter(todo => todo.done);
-            let undone = state.todos.filter(todo => !todo.done);
-
-            undone = undone.sort((a, b) => {
-                let aTime = (new Date(a.date)).getTime();
-                let bTime = (new Date(b.date)).getTime();
-                return bTime - aTime;
-            });
-
-            done = done.sort((a, b) => {
-                let aTime = (new Date(a.date)).getTime();
-                let bTime = (new Date(b.date)).getTime();
-                return bTime - aTime;
-            });
+            state.todos.push(action.payload);
 
             return {
                 ...state,
-                todos: [newTodo, ...undone, ...done]
+                todos: sortTodos(state.todos)
             };
         };
 
         case REMOVE_TODO: {
             const removedId = action.payload;
             state.todos = state.todos.filter(todo => todo.id !== removedId);
+
             return { ...state };
         };
 
         case TOGGLE_TODO: {
             const toggleId = action.payload.id;
-
             state.todos = state.todos.map(todo => (
                 todo.id === toggleId ?
                 { ...todo, done: !todo.done } :
                 todo
             ));
 
-            let done = state.todos.filter(todo => todo.done);
-            let undone = state.todos.filter(todo => !todo.done);
-            
-            done = done.sort((a, b) => {
-                let aTime = (new Date(a.date)).getTime();
-                let bTime = (new Date(b.date)).getTime();
-                return bTime - aTime;
-            });
-
-            undone = undone.sort((a, b) => {
-                let aTime = (new Date(a.date)).getTime();
-                let bTime = (new Date(b.date)).getTime();
-                return bTime - aTime;
-            });
-
             return {
                 ...state,
-                todos: [...undone, ...done]
+                todos: sortTodos(state.todos)
             };
         };
 
