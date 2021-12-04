20dc50031d5123bf25fa4e5ae556d0926865c39c
GrimGuyMike <ggm_job@mail.ru>
2021-12-04 14:51:31 +0500

    removed cors

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
-const apiRouter = require('./route/api');
-const path = require('path');
-const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const apiRouter = require('./route/api');
 const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 app.use(logger);
 
 if(process.env.NODE_ENV === 'production') {
